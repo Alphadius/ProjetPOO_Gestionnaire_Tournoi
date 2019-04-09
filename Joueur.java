635160e61695c77f7b98181988e51b154987df3b
Alphadius <36196483+Alphadius@users.noreply.github.com>
2019-04-09 10:24:56 +0200

    Add files via upload

---
+++ Joueur.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+
+public class Joueur extends Personne {
+
+  public Position String;
+
+  public titulaire boolean;
+
+    public Vector  myEquipe;
+    public Vector  myStatJoueur;
+    public Vector  myEquipe;
+    public Vector  myEquipe;
+    /**
+   * 
+   * @element-type Participation
+   */
+  public Vector  tttt;
+    public Vector  myPersonne;
+    public Equipe appartient;
+
+  public void afficherStatJoueur() {
+  }
+
+  public void afficherJoueur() {
+  }
+
+}
