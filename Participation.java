1b9409b22268747d16d1617a60887067b02dc221
dianausse <49432046+dianausse@users.noreply.github.com>
2019-04-18 10:26:02 +0100

    Update Participation.java
    Creation entrerScore()

--- Participation.java
+++ Participation.java
@@ -1,8 +1,23 @@
-import java.util.Vector;
+import java.util.*;
 
 public class Participation {
 
-  int nbBut;
+  int nbButs[][] = new int[nbEquipes][/*nbMatchs*/]; //tableau contenant le nombre de buts par équipe : nbButs[idEquipe][idMatch]
+  int idEquipe, idMatch;
+  
+  public void entrerScore() {
+    Scanner reader = new Scanner(System.in);
+      
+    System.out.println("Entrer le n° identifiant du match");
+    idMatch = reader.nextInt();
+    
+    for(int i=0; i<2; i++) {
+      System.out.println("Entrer le n° de l'équipe\n");
+      idEquipe = reader.nextInt();
+      System.out.println("Entrer le nombre de buts de l'équipe");
+      nbButs[idEquipe][idMatch] = reader.nextInt(); 
+    }
+  }
 
   int nbCarton;
 
