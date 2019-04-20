851aa572e69583173ea895796dc205fcf52ca141
Alphadius <36196483+Alphadius@users.noreply.github.com>
2019-04-20 15:59:49 +0200

    Update Competition.java

--- Competition.java
+++ Competition.java
@@ -1,22 +1,20 @@
-import java.util.Vector;
+import java.util.*;
 
 public class Competition {
 
-String NomCompetiton;
+  String NomCompetiton;
 
- int NombreEquipes;
+  int NombreEquipes;
 
- int dateDeDebut;
+  int dateDeDebut;
 
-    public Vector  myMatch;
-    public Match 1.n;
-    public Vector  1.n;
-    public Vector  myMatch;
-    /**
+  public Vector myMatch;public Match 1. n;public Vector 1. n;
+  public Vector myMatch;
+  /**
    * 
    * @element-type Match
    */
-  public Vector  participe;
+  public Vector participe;
 
   public void afficherMatchAll() {
   }
@@ -28,6 +26,30 @@ String NomCompetiton;
   }
 
   public void creationCompetition() {
+
+  public static void main(String[] args) {
+    int nbEquipes = 0;
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Quel est le nombre d'equipes ?");
+    nbEquipes = reader.nextInt();
+    String tab[] = new String[nbEquipes];
+    for (int i = 0; i < nbEquipes; i++) {
+      Scanner lire = new Scanner(System.in);
+      System.out.println("Choisisez le nom de l'Equipe numero " + (i + 1));
+      tab[i] = lire.nextLine();
+    }
+    int tour = 0;
+    for (int i = 0; i < nbEquipes; i++) {
+      for (int j = 1; j < nbEquipes; j++) {
+        if (i + j < nbEquipes) {
+          String match = tab[i] + " - " + tab[i + j];
+          System.out.println(match);
+          tour++;
+          System.out.println(tour);
+        }
+      }
+    }
   }
+}
 
 }
