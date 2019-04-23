86b6f3734f4afa97982e3041a21d6d66226f3529
Trithuan <trithuan91@gmail.com>
2019-04-23 10:49:51 +0200

    reset grammaire

--- Joueur.java
+++ Joueur.java
@@ -2,17 +2,17 @@ import java.util.Vector;
 
 public class Joueur extends Personne {
 
-  String position;
-  String equipe;
+  public int position;
+  public String equipe;
+  public int dateNaissance;
   public boolean titulaire;
   public int numeroJoueur;
-  public float vitesse;
+  public int vitesse;
   public int tirs;
   public int passes;
   public int dribbles;
   public int defense;
   public int physique;
-  
 
   /*
     public Vector  myEquipe;
@@ -26,14 +26,26 @@ public class Joueur extends Personne {
   public Vector  tttt;
   public Vector  myPersonne;
   public Equipe appartient;*/
-
-  public Joueur(String nom, String prenom, int age, String dateNaissance, String position, String equipe, boolean titulaire, int numeroJoueur, float vitesse, int tirs, int passes, int dribbles, int defense, int physique) {
+  public Joueur() {
+    this.nom = "";
+    this.prenom = "";
+    this.dateNaissance = 0;
+    this.position = 0;
+    this.titulaire = false;
+    this.numeroJoueur = 0;
+    this.vitesse = 0;
+    this.tirs = 0;
+    this.passes = 0;
+    this.dribbles = 0;
+    this.defense = 0;
+    this.physique = 0;
+    this.equipe = "";
+  }
+  public Joueur(String nom, String prenom, int age, int dateNaissance, int position, String equipe, boolean titulaire, int numeroJoueur, int vitesse, int tirs, int passes, int dribbles, int defense, int physique) {
     this.nom = nom;
     this.prenom = prenom;
-    this.age = age;
     this.dateNaissance = dateNaissance;
     this.position = position;
-    this.equipe = equipe;
     this.titulaire = titulaire;
     this.numeroJoueur = numeroJoueur;
     this.vitesse = vitesse;
@@ -42,6 +54,7 @@ public class Joueur extends Personne {
     this.dribbles = dribbles;
     this.defense = defense;
     this.physique = physique;
+    this.equipe = equipe;
   }
 
   // il faudra créer les joueurs avec une commande du style : Joueur ronaldo = new Joueur("ronaldo", "cristiano", 34, "5 février 1985", .. .. .. ... etc avec les autres stats);
