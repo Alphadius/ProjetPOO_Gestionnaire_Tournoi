e7df70dcc4e3177c97d504c78fdf6884bff07150
Trithuan <trithuan91@gmail.com>
2019-04-24 21:05:54 +0200

    Create Equipes.java

---
+++ Equipes.java
@@ -0,0 +1,37 @@
+import java.util.Vector;
+
+public class Equipes {
+
+  String nomEquipe;
+  String nomCoach;
+  int nombreJoueurs;
+  //List<Joueur> JoueursInEquipe = new ArrayList<Joueur>();
+
+
+public Equipes(String nom){
+  this.nomEquipe = nom;
+}
+
+
+public Equipes() {
+  this.nomEquipe = "non attribuer";
+  this.nomCoach = "non attribuer";
+  this.nombreJoueurs = 0;
+  }
+
+  public void afficherStatEquipe() {
+    System.out.println(this.nomEquipe);
+    System.out.println("Coach : " + this.nomCoach);
+    System.out.println("Nombre de Joueurs : " + nombreJoueurs);
+    // Faire pareil pour les autres stats
+  }
+
+  public void changementFormation() {
+
+  }
+
+  public void afficherMatchEquipe() {
+    
+  }
+
+}
