5d4ae3a743c699f81b15dac2b7484c6a3ecca141
Kevin Matsumoto <kevinmatsumoto@MacBook-Pro-de-Kevin.local>
2019-04-28 18:44:16 +0200

    Centrage du bouton Start/Nouveau tournoi

--- menu.java
+++ menu.java
@@ -9,11 +9,19 @@ import java.awt.Dimension;
 public class menu{
 	public menu(JFrame fenetre,Competition comp){
 
+		int largeurFrame, hauteurFrame, largeurButton, hauteurButton, x, y;
+		largeurFrame = 1000;
+		hauteurFrame = 600;
+		largeurButton = 300;
+		hauteurButton = 100;
+		x = (largeurFrame/2) - (largeurButton/2);
+		y = (hauteurFrame/2) - (hauteurButton/2);
+
 		JButton start = new JButton("Nouveau Tournoi");
-		start.setBounds(300,300,300,100); 
+		start.setBounds(x, y,largeurButton, hauteurButton); 
 		fenetre.add(start); 
 		fenetre.setLayout(null);
-	    fenetre.setPreferredSize(new Dimension(1000, 600));
+	    fenetre.setPreferredSize(new Dimension(largeurFrame, hauteurFrame));
 	    fenetre.setVisible(true);
 		fenetre.pack();
 
