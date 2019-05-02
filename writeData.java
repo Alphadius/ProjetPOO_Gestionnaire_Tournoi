527301db22e48c18c257c87d3d71325b06e96659
Trithuan <trithuan91@gmail.com>
2019-05-02 11:50:23 +0200

    test writedata creation fichier

--- writeData.java
+++ writeData.java
@@ -4,7 +4,7 @@ import java.io.PrintWriter;
 public class writeData{
 	public static void main(String[] args) {
 
-		String fileName = "sauvegarde.txt";
+		String fileName = "testdestrucs.txt";
 
 		PrintWriter writer = null;
 		try{
